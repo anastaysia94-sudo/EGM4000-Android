f75632995bd6e1038616bf2ea9ae940c4b8b2371
anastaysia94-sudo <anastaysia94@gmail.com>
2026-09-05 17:17:24 -0700

    Add EGM4000 web service worker

---
+++ web/sw.js
@@ -0,0 +1,13 @@
+const CACHE = 'egm4000-public-beta-v1';
+const ASSETS = ['./', './index.html', './manifest.webmanifest'];
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
+});
+self.addEventListener('fetch', event => {
+  const url = new URL(event.request.url);
+  if (url.pathname.startsWith('/api/')) return;
+  event.respondWith(caches.match(event.request).then(hit => hit || fetch(event.request)));
+});
