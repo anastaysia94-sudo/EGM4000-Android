457f3ab21e65952fa18d8462138269588429d056
anastaysia94-sudo <anastaysia94@gmail.com>
2026-09-05 17:15:55 -0700

    Add EGM4000 Android app Gradle build

---
+++ android-egm4000/app/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.smartpickshop.egm4000"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.smartpickshop.egm4000"
+        minSdk = 26
+        targetSdk = 36
+        versionCode = 1
+        versionName = "0.1.0-fire-kirin-companion"
+    }
+}
