7d01cfb2ea82e215e10c5f44edd9ea82d2bbe7b1
anastaysia94-sudo <anastaysia94@gmail.com>
2026-09-05 19:20:31 -0700

    Bump EGM4000 web cache for C004 community

--- web/sw.js
+++ web/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'egm4000-public-beta-v1';
+const CACHE = 'egm4000-public-beta-v2-c004';
 const ASSETS = ['./', './index.html', './manifest.webmanifest'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
