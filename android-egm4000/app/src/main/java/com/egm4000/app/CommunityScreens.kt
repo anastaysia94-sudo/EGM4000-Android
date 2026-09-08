df582d9fdeee931eff6c39f522c0b4897c616df3
anastaysia94-sudo <314408651+anastaysia94-sudo@users.noreply.github.com>
2026-09-08 03:30:57 -0700

    Fix Compose layout scope imports in community screens

--- android-egm4000/app/src/main/java/com/egm4000/app/CommunityScreens.kt
+++ android-egm4000/app/src/main/java/com/egm4000/app/CommunityScreens.kt
@@ -1,10 +1,7 @@
 package com.egm4000.app
 
 import android.content.Context
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.weight
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Button
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.OutlinedButton
